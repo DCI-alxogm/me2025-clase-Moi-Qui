239cfd778b158666ceea81539e34b710caaca17d
Moi-Qui <hernandezmoi715@gmail.com>
2025-08-08 13:42:31 -0400

    Add files via upload

---
+++ semana1/ejercicio1_Ago.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+  float numeros[5];
+  numeros[0]=3.2;
+  numeros[1]=4.2;
+  numeros[2]=5.7;
+  numeros[3]=5.2;
+  numeros[4]=6.4;
+printf ("%f %f %f %f %f\n",numeros[5]);
+}
